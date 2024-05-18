6b2c30fa4a9b3e6ac51afbafb5453b1c6ddbf961
Sul <76082727+Sv6@users.noreply.github.com>
2024-05-18 22:04:47 +0300

    deploy left

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Usecase-6 (Project-3)
+
+## Streamlit link:
+
+
--- str.py
+++ str.py
@@ -27,17 +27,19 @@ chart = pd.value_counts(s).to_frame(name='data')
 chart['percent'] = (chart['data'] / chart['data'].sum()) * 100
 chart.index.name = 'labels'
 chart = chart.reset_index().sort_values(['data', 'labels'], ascending=[False, True])
-chart = chart[:100]
+chart = chart[:20]
 charts = [go.Bar(x=chart['labels'].values, y=chart['data'].values, name='Frequency')]
 figure = go.Figure(data=charts, layout=go.Layout({
     'barmode': 'group',
     'legend': {'orientation': 'h'},
-    'title': {'text': ' '},
+    'title': {'text': 'neighbourhood Value Counts'},
     'xaxis': {'title': {'text': 'neighbourhood'}},
     'yaxis': {'title': {'text': 'Frequency'}}
 }))
 st.subheader('Neighbourhood Value Counts Visualization')
 st.plotly_chart(figure)
+
+st.write("The chart shows the mix of old and new neighborhoods. New sections feature modern villas, while established areas see long-term residents moving out. Infrastructure is improving with new amenities and services for the growing population. This combination of tradition and innovation makes the area attractive to potential buyers.")
 
 st.sidebar.title("Filters")
 
@@ -49,35 +51,37 @@ selected_region = st.sidebar.selectbox("Select Region", region_options)
 neighborhood_options = list(df[df['location'] == selected_region]['neighbourhood'].unique())
 selected_neighborhood = st.sidebar.selectbox("Select Neighborhood", neighborhood_options)
 
-minprice = int(df[df['neighbourhood'] == selected_neighborhood]['price'].min())
+minprice = int(df[df['neighbourhood'] == selected_neighborhood]['price'].min()) - 1
 maxprice = int(df[df['neighbourhood'] == selected_neighborhood]['price'].max())
-minspace = int(df[df['neighbourhood'] == selected_neighborhood]['space'].min())
+minspace = int(df[df['neighbourhood'] == selected_neighborhood]['space'].min()) - 1
 maxspace = int(df[df['neighbourhood'] == selected_neighborhood]['space'].max())
 
 medprice = int(df[df['neighbourhood'] == selected_neighborhood]['price'].median())
 medspace = int(df[df['neighbourhood'] == selected_neighborhood]['space'].median())
 
-price_min = st.sidebar.slider("Minimum Price (SAR)", minprice - 1, maxprice, medprice//2)
-price_max = st.sidebar.slider("Maximum Price (SAR)", minprice - 1, maxprice, medprice)
-size_min = st.sidebar.slider("Minimum Size (sqm)", minspace - 1, maxspace, medspace//2) 
-size_max = st.sidebar.slider("Maximum Size (sqm)", minspace - 1, maxspace, medspace)
-kitchen_ch = st.sidebar.checkbox("Kitchen")
+price_range = st.sidebar.slider("Price (SAR)", minprice, maxprice, (minprice, maxprice))
+size_range = st.sidebar.slider("Size (sqm)", minspace, maxspace, (minspace, maxspace)) 
+furnished_ch = st.sidebar.checkbox("Furnished")
 garage_ch = st.sidebar.checkbox("Garage")
 elevator_ch = st.sidebar.checkbox("Elevator")
-maidroom_ch = st.sidebar.checkbox("Maidroom")
+maidroom_ch = st.sidebar.checkbox("Maid Room")
+driver_ch = st.sidebar.checkbox("Driver Room")
 pool_ch = st.sidebar.checkbox("Pool")
 basement_ch = st.sidebar.checkbox("Basement")
 
 
 filtered_df = df[(df['neighbourhood'] == selected_neighborhood) &
-                    (df['price'] >= price_min) & (df['price'] <= price_max) &
-                    (df['space'] >= size_min) & (df['space'] <= size_max) & (df['kitchen'] == kitchen_ch) & (df['garage'] == garage_ch)
-                    & (df['elevator'] == elevator_ch) & (df['maidRoom'] == maidroom_ch) & (df['pool'] == pool_ch) & (df['basement'] == basement_ch)]
+                    (df['price'] >= price_range[0]) & (df['price'] <= price_range[1]) &
+                    (df['space'] >= size_range[0]) & (df['space'] <= size_range[1]) & 
+                    (df['furnished'] == furnished_ch) & (df['garage'] == garage_ch) & 
+                    (df['elevator'] == elevator_ch) & (df['maidRoom'] == maidroom_ch) & 
+                    (df['pool'] == pool_ch) & (df['basement'] == basement_ch) & 
+                    (df['driverRoom'] == driver_ch)]
 
 
       
 
-st.subheader(f"Neighborhoods with Villas in the Price Range of {price_min} SAR to {price_max} SAR and Size Range of {size_min} sqm to {size_max} sqm:")
+st.subheader(f"Neighborhoods with Villas in the Price Range of {price_range[0]:,} SAR to {price_range[1]:,} SAR and Size Range of {size_range[0]:,} sqm to {size_range[1]:,} sqm")
 st.write('To help the family in their search, we explore the villa market in Riyadh, focusing on important factors like price, size, and features. Using interactive tools and filters, we can find the best neighborhoods and villa options that meet their needs.')
 # st.write(filtered_df['neighbourhood'].unique())
 
@@ -103,15 +107,17 @@ st.write("The family can filter villas based on their budget and the required si
 st.write("By selecting different neighborhoods, the family can explore which areas offer villas within their desired price and size range.")
 
 st.subheader("Property Age Distribution by Neighborhood")
-fig = px.histogram(filtered_df, x='propertyAge', nbins=20, title=' ')
+fig = px.histogram(filtered_df, x='propertyAge', nbins=45, title=' ')
 
 fig.update_layout(
     xaxis_title='Property Age',
     yaxis_title='Count',
     bargap=0.1,
-    title_x=0.5 
+    title_x=0.5,
 ) 
 st.plotly_chart(fig)
+
+st.write('As we explore the age of the villa, we notice a distinct pattern. In older neighbourhoods, there\'s a prevalence of villas reflecting the longstanding history and heritage of these areas. Conversely, in newer neighbourhoods, the trend is different, with villas predominantly younger than the age of the villa in older neighbourhoods.')
 
 
 st.subheader("Conclusion")
